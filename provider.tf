2a949f1e6ea9f16ce28eddccf78a615d3cd84d21
UrsMartin <86231709+UrsMartin@users.noreply.github.com>
2021-06-21 15:23:51 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  subscription_id = "2a41d326-5d9d-493d-8c2d-69fd63e30c8b"
+  tenant_id = "eefbca93-524c-4293-a657-a7ecf59afeda"
+  client_id = "5c469533-1d7c-496d-b9e7-ded93695e148"
+  features{}
+}
